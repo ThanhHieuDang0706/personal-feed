8781387095c9619fa2cfaef609fdddd91e4380cb
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-11-22 11:43:11 +0700

    add data

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
 import { verifyToken } from './middleware/auth.js';
 import { createPost } from './controllers/posts.js';
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts } from './data/index.js';
 
 // ------------------ MIDDLEWARES CONFIG  ------------------
 const __filename = fileURLToPath(import.meta.url);
@@ -56,5 +59,10 @@ mongoose
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+        // add data one time only yeah
+        // User.insertMany(users);
+        // Post.insertMany(posts);
+    })
     .catch((error) => console.error(`${error} did not connect!`));
