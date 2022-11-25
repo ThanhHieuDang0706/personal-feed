424ab5601aba80c75ae73d57aea5733a6c8d8d33
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-11-25 19:23:33 +0700

    update code

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import HomePage from 'scenes/homePage';
 import LoginPage from 'scenes/profilePage';
 import ProfilePage from 'scenes/profilePage';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme.js';
 
 function App() {
-    const mode = useState();
+    const mode = useSelector(state => state.mode);
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
     return <div className='App'>
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<LoginPage />} />
-                <Route path='/home' element={<HomePage />} />
-                <Route path='/profile/:userId' element={<ProfilePage />} />       
-            </Routes>
+            <ThemeProvider theme={theme}>
+                <CssBaseline>
+                    <Routes>
+                        <Route path='/' element={<LoginPage />} />
+                        <Route path='/home' element={<HomePage />} />
+                        <Route path='/profile/:userId' element={<ProfilePage />} />       
+                    </Routes>
+                </CssBaseline>
+            </ThemeProvider>
         </BrowserRouter>
     </div>;
 }
