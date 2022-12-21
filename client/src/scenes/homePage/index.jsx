24d8d6238a4b467c3c94e29fb085bdb260dcec85
Thanh Hieu <49335345+ThanhHieuDang0706@users.noreply.github.com>
2022-12-21 20:51:06 +0700

    add post list

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import NavBar from 'scenes/navBar';
 import UserWidget from 'scenes/widgets/UserWidget';
 import MyPostWidget from 'scenes/widgets/MyPostWidget';
+import PostsWidget from 'scenes/widgets/PostsWidget';
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
@@ -12,12 +13,19 @@ const HomePage = () => {
   return (
     <Box>
       <NavBar />
-      <Box width="100%" padding="2rem 6%" display={isNonMobileScreens ? 'flex' : 'block'} gap="0.5rem" justifyContent="space-between">
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? 'flex' : 'block'}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
         <Box flexBasis={isNonMobileScreens ? '26%' : undefined}>
           <UserWidget userId={_id} picturePath={picturePath} />
         </Box>
         <Box flexBasis={isNonMobileScreens ? '42%' : undefined} mt={isNonMobileScreens ? undefined : '2rem'}>
           <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
         </Box>
 
         {isNonMobileScreens && <Box flexBasis="26%"></Box>}
